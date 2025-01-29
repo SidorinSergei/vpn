30eb8dfe9e53b48a45c95dccec1a30858bccfd66
SidorinSergei <95975705+SidorinSergei@users.noreply.github.com>
2025-01-29 03:23:08 +0300

    the test ovpn is connected and configured

--- android/app/src/main/kotlin/com/example/vpn/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vpn/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.vpn
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent
+import id.laskarmedia.openvpn_flutter.OpenVPNFlutterPlugin
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        OpenVPNFlutterPlugin.connectWhileGranted(requestCode == 24 && resultCode == RESULT_OK)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,122 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:openvpn_flutter/openvpn_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late OpenVPN engine;
+  VpnStatus? status;
+  String? stage;
+  bool _granted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    engine = OpenVPN(
+      onVpnStatusChanged: (data) {
+        print("VPN Status: ${data?.toJson()}");
+        setState(() {
+          status = data;
+        });
+      },
+      onVpnStageChanged: (data, raw) {
+        print("VPN Stage Changed: $data | Raw: $raw");
+        setState(() {
+          stage = raw;
+        });
+      },
+    );
+
+    engine.initialize(
+      groupIdentifier: "group.com.example.vpn",
+      providerBundleIdentifier: "com.example.vpn",
+      localizedDescription: "VPN Connection",
+      lastStage: (stage) {
+        setState(() {
+          this.stage = stage.name;
+        });
+      },
+      lastStatus: (status) {
+        setState(() {
+          this.status = status;
+        });
+      },
+    );
+  }
+
+  Future<void> initPlatformState() async {
+    try {
+      print("Loading VPN configuration...");
+      String config = await rootBundle.loadString('assets/ovpn_configs/nl-free-91.protonvpn.tcp.ovpn');
+
+      print("Starting VPN connection...");
+      await engine.connect(
+        config,
+        "VPN Server",
+        username: "xBb3HB9o0uJpuN4J",
+        password: "rbF6tmu4RCXxpUEBCcFHNgoPMmc4c2hX",
+        certIsRequired: true,
+      );
+      print("VPN connection started.");
+    } catch (e) {
+      print("Error during VPN connection: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('VPN Example App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("VPN Stage: ${stage ?? 'Unknown'}"),
+              Text("VPN Status: ${status?.toJson().toString() ?? 'Unknown'}"),
+              TextButton(
+                child: const Text("Start"),
+                onPressed: () {
+                  print("Start button pressed.");
+                  initPlatformState();
+                },
+              ),
+              TextButton(
+                child: const Text("STOP"),
+                onPressed: () {
+                  print("VPN Disconnecting...");
+                  engine.disconnect();
+                },
+              ),
+              if (Platform.isAndroid)
+                TextButton(
+                  child: Text(_granted ? "Granted" : "Request Permission"),
+                  onPressed: () {
+                    engine.requestPermissionAndroid().then((value) {
+                      setState(() {
+                        _granted = value;
+                      });
+                    });
+                  },
+                ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
